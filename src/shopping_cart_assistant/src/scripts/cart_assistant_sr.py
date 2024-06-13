8589fa0fbc6efc913098b91c53a06c92798181e2
Ivan Khirusman <121748721+Vanstage@users.noreply.github.com>
2024-06-13 16:17:46 +0800

    Update cart_assistant_sr.py

--- src/shopping_cart_assistant/src/scripts/cart_assistant_sr.py
+++ src/shopping_cart_assistant/src/scripts/cart_assistant_sr.py
@@ -8,22 +8,23 @@ def googlesr():
     pub = rospy.Publisher('result', String, queue_size=10)
 
     while not rospy.is_shutdown():
-        # obtain audio from the microphone
+        # Obtain audio from the microphone
         r = sr.Recognizer()
         
         with sr.Microphone() as source:
             print(">>> Say something!")
-            #audio = r.listen(source)
             audio = r.record(source, duration=5)
             
-        # recognize speech using Google Speech Recognition
+        # Recognize speech using Google Speech Recognition
         try:
             result = r.recognize_google(audio, language="en-US")
             print("SR result: " + result)
             pub.publish(result)
         except sr.UnknownValueError:
+            pub.publish("I did not understand that.")
             print("SR could not understand audio")
         except sr.RequestError as e:
+            pub.publish("Request to Google Speech Recognition failed.")
             print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 if __name__ == '__main__':
