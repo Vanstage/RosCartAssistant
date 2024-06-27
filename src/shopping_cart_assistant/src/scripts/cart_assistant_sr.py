b4265012154b75c8765b4bca08ebeeaf7d253198
fakhrulfaiz <116660068+fakhrulfaiz@users.noreply.github.com>
2024-06-27 14:35:19 +0800

    Fix no input timer timout

--- src/shopping_cart_assistant/src/scripts/cart_assistant_sr.py
+++ src/shopping_cart_assistant/src/scripts/cart_assistant_sr.py
@@ -14,7 +14,8 @@ class SpeechRecognitionNode:
         self.detected_objects = []
         self.repeat_speech = ""
         self.price_dict = price_dict
-
+        self.no_input_timer = None
+        self.no_input_timeout = rospy.Duration(10)  # Timeout duration in seconds
         rospy.Subscriber('image_detection_result', String, self.object_detection_callback)
         self.recognizer = sr.Recognizer()
 
@@ -37,6 +38,9 @@ class SpeechRecognitionNode:
                     rospy.loginfo(">>> Say something!")
                     audio = self.recognizer.listen(source)
                 
+                if not self.no_input_timer:
+                    self.no_input_timer = rospy.Timer(self.no_input_timeout, self.no_input_timeout_callback)
+
                 try:
                     result = self.recognizer.recognize_google(audio, language="en-US")
                     rospy.loginfo("SR result: " + result)
@@ -50,22 +54,33 @@ class SpeechRecognitionNode:
                 # Clear detected_objects after processing
                 self.detected_objects = []
             else:
+                if not self.no_input_timer:
+                    self.no_input_timer = rospy.Timer(self.no_input_timeout, self.no_input_timeout_callback)
                 rospy.loginfo("Waiting for new detected objects...")
                 rospy.sleep(1)  
 
+    def no_input_timeout_callback(self, event):
+        text = "Do you like to place the item in your cart?"
+        rospy.loginfo(text)
+        handle_speech_result(text)
+        self.no_input_timer = None  # Reset the timer
+        
     def handle_speech_result(self, result):
         if "price" in result.lower():
+            check_input_timer()
             object_info_list = ["{} with price ${}".format(obj, self.price_dict.get(obj, 'unknown')) for obj in self.detected_objects[:5]]
             object_info = ', '.join(object_info_list)
             text = "Detected objects: {}".format(object_info)
             self.repeat_speech = text
             self.pub_tts.publish(text)
         elif "repeat" in result.lower():
+            check_input_timer()
             if self.repeat_speech != "":
                 self.pub_tts.publish(self.repeat_speech)
             else:
                 self.pub_tts.publish("No text to be repeated")
         elif "add to cart" in result.lower():
+            check_input_timer()
             for obj in self.detected_objects[:5]:
                 if obj in self.price_dict:
                     self.cart.append(obj)
@@ -74,6 +89,7 @@ class SpeechRecognitionNode:
             self.pub_tts.publish("Items added to cart")
         
         elif "list item" in result.lower():
+            check_input_timer()
             if self.cart:
                 cart_content = ','.join(self.cart)
                 self.pub_cart.publish(cart_content)
@@ -85,6 +101,7 @@ class SpeechRecognitionNode:
         
         
         elif "total" in result.lower():
+            check_input_timer()
             total_price = 0.0
             for item in self.cart:
                 total_price += self.price_dict.get(item, 0)
@@ -100,6 +117,11 @@ class SpeechRecognitionNode:
         else:
             self.pub_tts.publish("I did not understand that. Please repeat.")
 
+    def check_input_timer(self):
+        if self.no_input_timer:
+            self.no_input_timer.shutdown()
+            self.no_input_timer = None
+            
     def run(self):
         self.listen_for_commands()  # Start listening for commands
 
